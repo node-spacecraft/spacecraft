0215d27c77ede95a28cec16a93dca4037149c7fd
moonrailgun <moonrailgun@gmail.com>
2017-09-06 16:06:54 +0800

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+spacecraft v1
+  a basic/light node server framework
+author: moonrailgun(moonrailgun@gmail.com)
+*/
+
+module.exports = require('./lib/application.js');
+exports.component = require('./lib/component.js');
